487c475cd2607e969377db9567440295e8994a24
bernhard.behrendt <bernhard.bezdek@gmail.com>
2020-01-02 15:54:03 +0100

    #11 Add Portuguese translations
    Issue: https://github.com/AOEpeople/cookie-notice/issues/11

--- src/cookie.notice.js
+++ src/cookie.notice.js
@@ -21,7 +21,8 @@
             it: 'Utilizziamo i cookie per essere sicuri che tu possa avere la migliore esperienza sul nostro sito. Se continui ad utilizzare questo sito assumiamo che tu ne sia felice.',
             en: 'We use cookies to ensure that you have the best experience on our website. If you continue to use this site we assume that you accept this.',
             de: 'Wir verwenden Cookies um sicherzustellen, dass Sie das beste Erlebnis auf unserer Website haben.',
-            fr: 'Nous utilisons des cookies afin d\'être sûr que vous pouvez avoir la meilleure expérience sur notre site. Si vous continuez à utiliser ce site, nous supposons que vous acceptez.'
+            fr: 'Nous utilisons des cookies afin d\'être sûr que vous pouvez avoir la meilleure expérience sur notre site. Si vous continuez à utiliser ce site, nous supposons que vous acceptez.',
+            pt: 'Utilizamos cookies para garantir que você tenha a melhor experiência em nosso site. Se você continuar a usar este site, assumimos que você aceita isso.'
         },
 
         cookieNoticePosition: 'bottom',
@@ -34,7 +35,8 @@
             it: 'Saperne di più',
             en: 'Learn more',
             de: 'Mehr erfahren',
-            fr: 'En savoir plus'
+            fr: 'En savoir plus',
+            pt: 'Saber mais'
         },
 
         buttonLocales: {
